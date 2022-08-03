06cbf27fb6a4138ed28ebe3d723f95222389ecf2
Uğur <ugurpekesensteam@gmail.com>
2022-08-03 17:33:03 +0300

    Files updated

--- xaron_notify/fxmanifest.lua
+++ xaron_notify/fxmanifest.lua
@@ -2,14 +2,13 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'XARON'
-description 'Just another notify plugin (:'
+description 'Just another notify script.'
 version '1.0.0'
 
 ui_page "ui/ui.html"
 
 files({
-    "ui/ui.html",
-    "ui/ui.css"
+    "ui/ui.html"
 })
 
 client_script {"client.js"}
